5cf97564650f88ba2d1c8c7c6900c35b48c2d26b
Abhay Doke <abhaydoke09@gmail.com>
2017-02-27 16:16:37 -0500

    added code for reading commands.txt from command_parser.py

---
+++ command_parser.py
@@ -0,0 +1,37 @@
+"""
+Handles the work of validating and processing command input.
+"""
+import os
+
+
+def get_valid_commands(queue, fi):
+    # TODO: efficiently evaluate commands
+    valid_commands = []
+    command_list = []
+    flag = -1
+    try:
+    	with open(fi) as f:
+    		for line in f:
+    			line = line.replace('\n','')
+    			if(line):
+	    			if line == '[COMMAND LIST]':
+	    				flag = 0
+	    			elif line == '[VALID COMMANDS]':
+	    				flag = 1
+	    			else:
+	    				if flag==0:
+	    					command_list.append(line)
+	    				else:
+	    					valid_commands.append(line)
+    except EnvironmentError as e:
+    	print e
+
+    print valid_commands
+    print command_list
+    
+    queue.put(command)
+
+
+def process_command_output(queue):
+    # TODO: run the command and put its data in the db
+    command = queue.get()
